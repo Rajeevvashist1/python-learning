792f08a30e36945b87fffcc5e91da8e3d166eb55
Rajeevvashist1 <vashistrajeev2611@gmail.com>
2026-01-28 21:51:45 +0530

    "Learned Basics of String "

--- Basics/Baiscs_03_Strings.py
+++ Basics/Baiscs_03_Strings.py
@@ -1,1 +1,29 @@
                                         #String
+
+#Let's take an string for more experimental studies 
+
+nums_string = "0123456789"
+# print(nums_string)
+
+# nums_string = nums_string.strip()
+# print(nums_string)
+
+# SLICING..............
+
+# print("nums_string[:]" , nums_string[:])
+# print("nums_string[0:]" , nums_string[0:])
+# print("nums_string[:-1]" , nums_string[:-1])
+# print("nums_string[-1:]", nums_string[-1:])
+# print("nums_string[1:5]", nums_string[1:5])
+# print("nums_string[2:8]" ,nums_string[2:8])
+
+# print(nums_string[0:10:2])
+
+# nums_string = nums_string.replace(nums_string[2] ,"2222222")
+# print(nums_string)
+
+# my_list = nums_string.split(",")
+# print(my_list)
+
+# print(nums_string.find("234"))     #Gives the index from where you can start finding the string you want
+
